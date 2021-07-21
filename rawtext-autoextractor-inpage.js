6cfb963c5493802a9e8307d7c320443ce7fa5e99
Eric <eiplugger@gmail.com>
2021-07-21 11:39:25 +0200

    📐 CODE IMPROVEMENTS => getWordsOccurencesReportHTML 2

--- rawtext-autoextractor-inpage.js
+++ rawtext-autoextractor-inpage.js
@@ -1,4 +1,5 @@
 //console.clear();
+let commonWords = JSON.parse(localStorage.getItem('raw-extract-common-words')) || []
 let separator = '===================================================================================';
 let rawArticle = '';
 let htmlTextsFromPage = extractPageText()
@@ -227,37 +228,44 @@ function getWordsOccurencesReportHTML() {
             let occurences = allWords.filter(y => y == x).length
             return {
                 text: x,
-                strongWord
+                occurences
             }
         })
         .sort((a, b) => b.occurences - a.occurences)
 
     let tableHtml = `
 <style type="text/css">
-.tg  {border-collapse:collapse;border-spacing:0;}
+.tg  {border-collapse:collapse;border-spacing:0;width:50%;margin:auto;}
 .tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
   overflow:hidden;padding:10px 5px;word-break:normal;}
 .tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
   font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-.tg .tg-symt{background-color:#000000;border-color:inherit;color:#000000;text-align:left;vertical-align:top}
-.tg .tg-0lax{text-align:left;vertical-align:top}
-.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+.tg .tg-10ea{background-color:#000000;border-color:inherit;color:#000000;
+  font-family:"Lucida Console", Monaco, monospace !important;;text-align:center;vertical-align:top}
+.tg .tg-n5dg{background-color:#ffccc9;border-color:inherit;font-family:"Lucida Console", Monaco, monospace !important;;
+  text-align:center;vertical-align:top}
+.tg .tg-igmv{background-color:#96fffb;border-color:inherit;font-family:"Lucida Console", Monaco, monospace !important;;
+  text-align:center;vertical-align:top}
+.tg .tg-xhg5{border-color:inherit;font-family:"Lucida Console", Monaco, monospace !important;;text-align:center;vertical-align:top}
+.tg .tg-5nj1{border-color:inherit;font-family:"Lucida Console", Monaco, monospace !important;;text-align:left;vertical-align:top}
 </style>
-<table class="tg">
+<table id="words-occurences-report-html" class="tg">
 <thead>
   <tr>
-    <th class="tg-0lax" colspan="2">STRONG WORDS</th>
-    <th class="tg-0lax"></th>
-    <th class="tg-0lax" colspan="2">COMMON WORDS</th>
+    <th class="tg-n5dg" colspan="3" width="40%">STRONG WORDS</th>
+    <th class="tg-xhg5" width="20%"></th>
+    <th class="tg-igmv" colspan="3" width="40%">COMMON WORDS</th>
   </tr>
 </thead>
 <tbody>
   <tr>
-    <td class="tg-0pky">Word</td>
-    <td class="tg-0pky">Occurences</td>
-    <td class="tg-symt"><span style="color:#FFF">Rank</span></td>
-    <td class="tg-0pky">Word</td>
-    <td class="tg-0pky">Occurences</td>
+    <td class="tg-n5dg">Word</td>
+    <td class="tg-n5dg">Occurences</td>
+    <td class="tg-n5dg">Action</td>
+    <td class="tg-10ea"><span style="color:#FFF">Rank</span></td>
+    <td class="tg-igmv">Action</td>
+    <td class="tg-igmv">Word</td>
+    <td class="tg-igmv">Occurences</td>
   </tr>
   {{ROW}}
 </tbody>
@@ -265,24 +273,25 @@ function getWordsOccurencesReportHTML() {
 
     let emptyTableRow = `
 <tr>
-    <td class="tg-0lax">ROW_1</td>
-    <td class="tg-0lax">ROW_2</td>
-    <td class="tg-0lax">ROW_3</td>
-    <td class="tg-0lax">ROW_4</td>
-    <td class="tg-0lax">ROW_5</td>
+    <td class="tg-n5dg">ROW_1</td>
+    <td class="tg-n5dg">ROW_2</td>
+    <td class="tg-n5dg"><button class="btn-words" data-text="ROW_1" data-occurences="ROW_2" data-action="add">➡</button></td>
+    <td class="tg-10ea"><span style="color:#FFF">ROW_3</span></td>
+    <td class="tg-igmv"><button class="btn-words" data-text="ROW_4" data-occurences="ROW_5" data-action="remove">❌</button></td>
+    <td class="tg-igmv">ROW_4</td>
+    <td class="tg-igmv">ROW_5</td>
 </tr>`
 
-
-    let commonWords = JSON.parse(localStorage.getItem('raw-extract-common-words')) || []
+    // inject full table with data
     let strongWords = occurencesReport.filter(x => commonWords.includes(x.text) == false)
     let htmlRowsAsText = ''
     for (let i = 0; i < Math.max(commonWords.length, strongWords.length); i++) {
         const commonWord = commonWords[i] || { text: '', occurences: '' };
         const strongWord = strongWords[i] || { text: '', occurences: '' };
         let newRow = emptyTableRow
-            .replace('ROW_1', commonWord.text).replace('ROW_2', commonWord.occurences)
-            .replace('ROW_3', i)
-            .replace('ROW_4', strongWord.text).replace('ROW_5', strongWord.occurences)
+            .replace(/ROW_1/g, strongWord.text).replace(/ROW_2/g, strongWord.occurences)
+            .replace(/ROW_3/g, i)
+            .replace(/ROW_4/g, commonWord.text).replace(/ROW_5/g, commonWord.occurences)
         htmlRowsAsText += newRow + '\n'
     }
 
@@ -293,6 +302,25 @@ function getWordsOccurencesReportHTML() {
     div.id = 'getWordsOccurencesReportHTML'
     div.innerHTML = tableHtml
     document.body.appendChild(div)
+    let btns = document.querySelectorAll('.btn-words')
+    btns.forEach(elm => {
+        elm.addEventListener('click', (e) => {
+            debugger
+            if (e.currentTarget.dataset.action == "add") {
+                // add the word to the "common list"
+                commonWords.push({ text: e.currentTarget.dataset.text, occurences: e.currentTarget.dataset.occurences })
+            } else if (e.currentTarget.dataset.action == "remove") {
+                // remove the word from the "common list"
+                commonWords.filter(x => x.text != e.currentTarget.dataset.text)
+            }
+            // remove table to refresh it
+            var element = document.getElementById("words-occurences-report-html");
+            element.parentNode.removeChild(element);
+            // call refresh
+            getWordsOccurencesReportHTML()
+        })
+
+    });
 
     console.table(occurencesReport)
 }
@@ -353,10 +381,10 @@ function injectButtonInHTML() {
             extractTextPortion("POSSESIFS", 'mon|ton|son|ma|ta|sa|mes|tes|ses|nos|vos')
             extractTextPortion("PREPOSITIONS", 'à|après|au|avant|avec|chez|contre|dans|de|depuis|derrière|devant|en|entre|envers|jusqu|malgré|par|pendant|pour|sans|sauf|sous|sur|vers')
             getWordsOccurencesReport()
-            getWordsOccurencesReportHTML()
             console.log(rawArticle);
 
             injectPreInHTML()
+            getWordsOccurencesReportHTML()
         }
         document.body.appendChild(btn)
     }
